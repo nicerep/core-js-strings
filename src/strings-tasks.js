aa45c9a0ec9f631dcfaa886a2f6d1bc8d06114d9
yourName <your@email.xxx>
2023-12-24 01:35:20 +0300

    feat: complete concatenateStrings; feat: complete getFirstChar;

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -39,6 +39,7 @@ function getStringLength(value) {
  *   isString(new String('test')) => true
  */
 function isString(/* value */) {
+  // return value instanceof String;
   throw new Error('Not implemented');
 }
 
@@ -54,8 +55,8 @@ function isString(/* value */) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return value1.concat(value2);
 }
 
 /**
@@ -69,8 +70,8 @@ function concatenateStrings(/* value1, value2 */) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value.charAt(0);
 }
 
 /**
